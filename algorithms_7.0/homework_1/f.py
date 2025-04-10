8a6b6a067267182c109fbc2f09cf350605fb0a7f
retrobich921 <albert1yandex@gmail.com>
2025-04-11 00:37:39 +0300

    + f

--- algorithms_7.0/homework_1/f.py
+++ algorithms_7.0/homework_1/f.py
@@ -1,17 +1,30 @@
+import copy
+
+
 def solve():
     N, M = map(int, input().split())
     m = list(map(int, input().split()))
     c = list(map(int, input().split()))
-    dp = [-1] * (M+1)
-    dp[0] = 0
-    mx = 0
-    for i in range(N):
-        for j in range(M, m[i]-1, -1):
-            if dp[j-m[i]] != -1:
-                dp[j] = max(dp[j], dp[j-m[i]] + c[i])
-                if dp[j] > mx:
-                    mx = dp[j]
-    print(mx)
+    bag = [[[-1, -1] for _ in range(M+1)] for _ in range(N+1)] #[[[ind_object, cost], [-1, -1], ..., [-1, -1]], [], []]
+    bag[0][0] = [0, 0]
+    ind_mx = 0
+    for i in range(1, N+1):
+        bag[i] = copy.deepcopy(bag[i-1]) # копируем предыдущие состояния, чтобы потом восстановить дорогу
+        for j in range(M, m[i-1]-1, -1):
+            if bag[i][j-m[i-1]][1] != -1: # цена не равняется -1, то есть там был какой то объект или там ничего не лежит
+                if bag[i][j][1] < bag[i][j-m[i-1]][1] + c[i-1]:
+                    bag[i][j] = [i, bag[i][j-m[i-1]][1] + c[i-1]]
+                    if bag[i][j][1] > bag[i][ind_mx][1]:
+                        ind_mx = j
+
+    ans = []
+    i = bag[N][ind_mx][0]
+    while ind_mx > 0:
+        ans += [bag[i][ind_mx][0]]
+        i = bag[i][ind_mx][0] - 1
+        ind_mx -= m[ans[-1]-1]
+
+    print(*reversed(ans), sep='\n')
 
 
 if __name__ == "__main__":
