37c42ae8b8a04b8fa85614a0e759781d4e7a8c63
retrobich921 <albert1yandex@gmail.com>
2025-04-23 11:32:16 +0300

    add D

--- algorithms_7.0/homework_2/d.py
+++ algorithms_7.0/homework_2/d.py
@@ -22,13 +22,17 @@ class SegmentTree():
         # print(*self.tree)
 
     def filling_ind_max(self):
-        for i in range((len(self.tree) >> 1) - 1, 0, -1):
-            if self.tree[i << 1 | 1][0] > self.tree[i << 1][0]:
-                self.tree[i] = self.tree[i << 1 | 1]
-            elif self.tree[i << 1 | 1][0] < self.tree[i << 1][0]:
-                self.tree[i] = self.tree[i << 1]
+        size = len(self.tree) >> 1
+        for i in range(size - 1, 0, -1):
+            left = self.tree[i << 1]       # ребёнок 2*i
+            right = self.tree[i << 1 | 1]  # ребёнок 2*i+1
+            if right[0] > left[0]:
+                self.tree[i] = right.copy()
+            elif right[0] < left[0]:
+                self.tree[i] = left.copy()
             else:
-                self.tree[i] = [self.tree[i << 1][0], self.tree[i << 1][1]]
+                # при равенстве берём значение + индекс левого
+                self.tree[i] = [left[0], left[1]]
 
     def query_max_count(self, node, left, right, l, r):
         if r <= left or right <= l:
@@ -49,32 +53,46 @@ class SegmentTree():
             return [left_result[0], left_result[1] + right_result[1]]
         
     def query_ind_max(self, node, left, right, l, r):
+        # не пересекаются
         if r <= left or right <= l:
             return [-float("inf"), 0]
 
         if l <= left and right <= r:
             return self.tree[node]
-
         mid = (left + right) >> 1
-        left_result = self.query_ind_max(node << 1, left, mid, l, r)
-        right_result = self.query_ind_max(node << 1 | 1, mid, right, l, r)
-
-        if left_result[0] > right_result[0]:
-            return left_result
-        elif right_result[0] > left_result[0]:
-            return right_result
+        L = self.query_ind_max(node << 1, left, mid, l, r)
+        R = self.query_ind_max(node << 1 | 1, mid, right, l, r)
+        if R[0] > L[0]:
+            return R
+        elif R[0] < L[0]:
+            return L
         else:
-            return [left_result[0], left_result[1]]
+            # при равенстве возвращаем индекс из левой части
+            return [L[0], L[1]]
 
     def upgrade_max(self, ind, value):
-        self.tree[ind] = [value, ind - (len(self.tree) >> 1)]
-        self.upgrade_max_ind(ind=ind)
+        self.tree[ind] = [value, ind - (len(self.tree) >> 1) + 1]
+        # протягиваем вверх
+        self.upgrade_max_ind(ind)
 
     def upgrade_max_ind(self, ind):
-        if 0 < (ind >> 1) < len(self.tree):
-            if self.tree[ind >> 1][0] < self.tree[ind][0]:
-                self.tree[ind >> 1] = self.tree[ind]
-                self.upgrade_max_ind(ind >> 1)
+        """Рекурсивно обновляем родителей, если они стали меньше потомка."""
+        parent = ind >> 1
+        if parent == 0:
+            return
+        left = self.tree[parent << 1]
+        right = self.tree[parent << 1 | 1]
+        # выбираем лучший из двух детей (левая сторона при равенстве)
+        if right[0] > left[0]:
+            best = right
+        else:
+            best = left
+        # если родитель уже равен best — можно остановиться
+        if self.tree[parent] == best:
+            return
+        self.tree[parent] = best.copy()
+        # идём дальше вверх
+        self.upgrade_max_ind(parent)
 
 
 def solve():
