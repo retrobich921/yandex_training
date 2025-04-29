9e34860feb94efea6ccc4efd8d634ac20c38b273
retrobich921 <albert1yandex@gmail.com>
2025-04-30 01:50:52 +0300

    реализована задача F, обновлен README.md для отметки выполнения

--- algorithms_7.0/homework_3/f.py
+++ algorithms_7.0/homework_3/f.py
@@ -1,34 +1,47 @@
+import sys
+
+
 def solve():
-    n, k = map(int, input().split())
-    a = [list(map(int, input().split())) for _ in range(k)]
+    n, k = map(int, sys.stdin.readline().split())
+    log_block = 10
+    bits_in_block = 1 << log_block
+    cnt_blocks = (n + bits_in_block - 1) >> log_block
+    xy = [[False] * n for _ in range(n)]
+    xz = [[0] * cnt_blocks for _ in range(n)]
+    yz = [[0] * cnt_blocks for _ in range(n)]
 
-    xs = set()
-    ys = set()
-    zs = set()
-    xy = set()
-    xz = set() 
-    yz = set()
+    for i in range(k):
+        x, y, z = map(int, sys.stdin.readline().split())
+        x, y, z = x - 1, y - 1, z - 1
+        xy[x][y] = True
+        z_block = z >> log_block
+        z_pos = z & (bits_in_block - 1)
+        z_set = 1 << z_pos
+        xz[x][z_block] |= z_set
+        yz[y][z_block] |= z_set
+    last_bits = (cnt_blocks << log_block) - n
+    last_block_filler = ((1 << last_bits) - 1) << (bits_in_block - last_bits)
+    for i in range(n):
+        xz[i][cnt_blocks - 1] |= last_block_filler
+        yz[i][cnt_blocks - 1] |= last_block_filler
+    full_block_mask = (1 << bits_in_block) - 1
+    for x in range(n):
+        for y in range(n):
+            if not xy[x][y]:
+                for block in range(cnt_blocks):
+                    if xz[x][block] | yz[y][block] != full_block_mask:
+                        now_pos = 1
+                        for i in range(bits_in_block):
+                            if (xz[x][block] | yz[y][block]) & now_pos == 0:
+                                ans = (x+1, y+1, (block << log_block) + i + 1)
+                                if ans[2] <= n:
+                                    print('NO')
+                                    print(*ans)
+                                    return
+                                break
+                            now_pos <<= 1
+    print('YES')
 
-    for xi, yi, zi in a:
-        xy.add((xi, yi))
-        xz.add((xi, zi))
-        yz.add((yi, zi))
-
-    full_xy = len(xy) == n * n
-    full_xz = len(xz) == n * n
-    full_yz = len(yz) == n * n
-
-    if full_xy or full_xz or full_yz:
-        print("YES")
-    else:
-        for x in range(1, n+1):
-            for y in range(1, n+1):
-                for z in range(1, n+1):
-                    if (x, y) not in xy and (x, z) not in xz and (y, z) not in yz:     
-                        print("NO")
-                        print(x, y, z)
-                        return
-        print('YES')
 
 if __name__ == '__main__':
     solve()
